93718879aa028f7536e0ef8d3257c20af756a802
Sakina <sakina.nadiadi@gmail.com>
2021-08-11 17:37:25 -0400

    added media query

--- app.js
+++ app.js
@@ -68,3 +68,19 @@ window.onload = function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.01em solid #fff}";
   document.body.appendChild(css);
 };
+
+// form Submission
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   let myForm = document.getElementById("pizzaOrder");
+//   let formData = new FormData(myForm);
+//   fetch("/", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: new URLSearchParams(formData).toString(),
+//   })
+//     .then(() => console.log("Form successfully submitted"))
+//     .catch((error) => alert(error));
+// };
+// document.querySelector("form").addEventListener("submit", handleSubmit);
