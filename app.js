827cf7f088fe55d92cedd44b4fb9ec8ccb79eb50
Sakina <sakina.nadiadi@gmail.com>
2021-08-11 19:50:08 -0400

    added animation

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// AOS.init();
+
+// // You can also pass an optional settings object
+// // below listed default settings
+// AOS.init({
+//   // Global settings:
+//   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//   startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+//   initClassName: "aos-init", // class applied after initialization
+//   animatedClassName: "aos-animate", // class applied on animation
+//   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+//   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+//   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+//   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+//   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//   offset: 120, // offset (in px) from the original trigger point
+//   delay: 0, // values from 0 to 3000, with step 50ms
+//   duration: 400, // values from 0 to 3000, with step 50ms
+//   easing: "ease", // default easing for AOS animations
+//   once: false, // whether animation should happen only once - while scrolling down
+//   mirror: false, // whether elements should animate out while scrolling past them
+//   anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+// });
+
 // Hamburger Menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("nav ul ");
@@ -68,19 +93,3 @@ window.onload = function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.01em solid #fff}";
   document.body.appendChild(css);
 };
-
-// form Submission
-
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   let myForm = document.getElementById("pizzaOrder");
-//   let formData = new FormData(myForm);
-//   fetch("/", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: new URLSearchParams(formData).toString(),
-//   })
-//     .then(() => console.log("Form successfully submitted"))
-//     .catch((error) => alert(error));
-// };
-// document.querySelector("form").addEventListener("submit", handleSubmit);
